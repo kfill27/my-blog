5134990d73baa4d70956620a61cefebd5ef0102c
kfill27 <kfillmore27@gmail.com>
2015-04-14 16:22:18 -0400

    Add User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts
   get 'about' => 'pages#about'
+  # get 'sign_out' => 'users#sign_out'
+  # get 'sign_in' => 'users#sign_in'
+  # get 'sign_up' => 'users#sign_up'
   root 'pages#welcome'
 
   # The priority is based upon order of creation: first created -> highest priority.
