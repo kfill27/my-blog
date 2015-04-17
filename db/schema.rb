421997e860ba0439d92c94c38d40ff3f8c2d430f
kfill27 <kfillmore27@gmail.com>
2015-04-17 10:27:54 -0400

    Add admin and remove user ability to post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150410164814) do
+ActiveRecord::Schema.define(version: 20150416203304) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150410164814) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
